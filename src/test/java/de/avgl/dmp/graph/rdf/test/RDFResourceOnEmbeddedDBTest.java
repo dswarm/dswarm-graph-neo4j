a0fd6e886604a6b8b506875128384eb971037228
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-03-28 11:16:48 +0100

    added logging to tests + test clean-up

--- src/test/java/de/avgl/dmp/graph/rdf/test/RDFResourceOnEmbeddedDBTest.java
+++ src/test/java/de/avgl/dmp/graph/rdf/test/RDFResourceOnEmbeddedDBTest.java
@@ -13,6 +13,8 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 import org.junit.Test;
 import org.neo4j.server.NeoServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.io.Resources;
 import com.hp.hpl.jena.rdf.model.Model;
@@ -25,6 +27,8 @@ import de.avgl.dmp.graph.test.EmbeddedNeo4jTest;
 
 public class RDFResourceOnEmbeddedDBTest extends EmbeddedNeo4jTest {
 
+	private static final Logger	LOG	= LoggerFactory.getLogger(RDFResourceOnEmbeddedDBTest.class);
+
 	public RDFResourceOnEmbeddedDBTest() {
 
 		super("/ext");
@@ -33,26 +37,32 @@ public class RDFResourceOnEmbeddedDBTest extends EmbeddedNeo4jTest {
 	@Test
 	public void testPingToTestDB() throws IOException {
 
-		// POST the request
+		LOG.debug("start ping test for RDF resource at embedded DB");
+
 		final ClientResponse response = service().path("/rdf/ping").get(ClientResponse.class);
-		// System.out.println("Response Status : " + response.getStatus());
 
 		String body = response.getEntity(String.class);
 
-		// System.out.println("Response Status : " + body);
-
 		Assert.assertEquals("expected 200", 200, response.getStatus());
 		Assert.assertEquals("expected pong", "pong", body);
+
+		LOG.debug("finished ping test for RDF resource at embedded DB");
 	}
 
 	@Test
 	public void writeRDFToTestDB() throws IOException {
 
+		LOG.debug("start write test for RDF resource at embedded DB");
+
 		writeRDFToTestDBInternal(server);
+
+		LOG.debug("finished write test for RDF resource at embedded DB");
 	}
 
 	@Test
 	public void readRDFFromTestDB() throws IOException {
+
+		LOG.debug("start read test for RDF resource at embedded DB");
 
 		writeRDFToTestDBInternal(server);
 
@@ -67,24 +77,25 @@ public class RDFResourceOnEmbeddedDBTest extends EmbeddedNeo4jTest {
 		// POST the request
 		final ClientResponse response = service().path("/rdf/get").type(MediaType.APPLICATION_JSON_TYPE).accept("application/n-triples")
 				.post(ClientResponse.class, requestJsonString);
-		// System.out.println("Response Status : " + response.getStatus());
 
 		Assert.assertEquals("expected 200", 200, response.getStatus());
 
 		final String body = response.getEntity(String.class);
 
-		// System.out.println("Response body : " + body);
-
 		final InputStream stream = new ByteArrayInputStream(body.getBytes("UTF-8"));
 		final Model model = ModelFactory.createDefaultModel();
 		model.read(stream, null, "N-TRIPLE");
 
-		// System.out.println("number of statements = '" + model.size() + "'");
-
 		Assert.assertEquals("the number of statements should be 2601", 2601, model.size());
+
+		LOG.debug("read '" + model.size() + "' statements");
+
+		LOG.debug("finished read test for RDF resource at embedded DB");
 	}
 
 	private void writeRDFToTestDBInternal(final NeoServer server) throws IOException {
+
+		LOG.debug("start writing RDF statements for RDF resource at embedded DB");
 
 		final URL fileURL = Resources.getResource("dmpf_bsp1.n3");
 		final byte[] file = Resources.toByteArray(fileURL);
@@ -96,10 +107,11 @@ public class RDFResourceOnEmbeddedDBTest extends EmbeddedNeo4jTest {
 
 		// POST the request
 		final ClientResponse response = service().path("/rdf/put").type("multipart/mixed").post(ClientResponse.class, multiPart);
-		// System.out.println("Response Status : " + response.getStatus());
 
 		Assert.assertEquals("expected 200", 200, response.getStatus());
 
 		multiPart.close();
+
+		LOG.debug("finished writing RDF statements for RDF resource at embedded DB");
 	}
 }
--- src/test/java/de/avgl/dmp/graph/rdf/test/RDFResourceOnRunningDBTest.java
+++ src/test/java/de/avgl/dmp/graph/rdf/test/RDFResourceOnRunningDBTest.java
@@ -12,19 +12,21 @@ import junit.framework.Assert;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.io.Resources;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.multipart.BodyPart;
 import com.sun.jersey.multipart.MultiPart;
 
 import de.avgl.dmp.graph.test.RunningNeo4jTest;
 
 public class RDFResourceOnRunningDBTest extends RunningNeo4jTest {
+
+	private static final Logger	LOG	= LoggerFactory.getLogger(RDFResourceOnRunningDBTest.class);
 
 	public RDFResourceOnRunningDBTest() {
 
@@ -34,32 +36,19 @@ public class RDFResourceOnRunningDBTest extends RunningNeo4jTest {
 	@Test
 	public void writeRDFToRunningDB() throws IOException {
 
-		final Client c = Client.create();
-		final WebResource service = c.resource("http://localhost:7474/graph");
+		LOG.debug("start write test for RDF resource at running DB");
 
-		final URL fileURL = Resources.getResource("dmpf_bsp1.n3");
-		final byte[] file = Resources.toByteArray(fileURL);
+		writeRDFToRunningDBInternal();
 
-		// Construct a MultiPart with two body parts
-		final MultiPart multiPart = new MultiPart();
-		multiPart.bodyPart(new BodyPart(file, MediaType.APPLICATION_OCTET_STREAM_TYPE)).bodyPart(
-				new BodyPart("http://data.slub-dresden.de/resources/2", MediaType.TEXT_PLAIN_TYPE));
-
-		// POST the request
-		final ClientResponse response = service.path("/rdf/put").type("multipart/mixed").post(ClientResponse.class, multiPart);
-		// System.out.println("Response Status : " + response.getStatus());
-
-		Assert.assertEquals("expected 200", 200, response.getStatus());
-
-		multiPart.close();
-
-		// TODO: do clean-up in DB
+		LOG.debug("finished write test for RDF resource at running DB");
 	}
 
 	@Test
 	public void readRDFFromRunningDB() throws IOException {
 
-		writeRDFToRunningDB();
+		LOG.debug("start read test for RDF resource at running DB");
+
+		writeRDFToRunningDBInternal();
 
 		final ObjectMapper objectMapper = new ObjectMapper();
 		final ObjectNode requestJson = objectMapper.createObjectNode();
@@ -72,35 +61,57 @@ public class RDFResourceOnRunningDBTest extends RunningNeo4jTest {
 		// POST the request
 		final ClientResponse response = service().path("/rdf/get").type(MediaType.APPLICATION_JSON_TYPE).accept("application/n-triples")
 				.post(ClientResponse.class, requestJsonString);
-		// System.out.println("Response Status : " + response.getStatus());
 
 		Assert.assertEquals("expected 200", 200, response.getStatus());
 
 		final String body = response.getEntity(String.class);
 
-		// System.out.println("Response body : " + body);
-
 		final InputStream stream = new ByteArrayInputStream(body.getBytes("UTF-8"));
 		final Model model = ModelFactory.createDefaultModel();
 		model.read(stream, null, "N-TRIPLE");
 
-		// System.out.println("number of statements = '" + model.size() + "'");
-
 		Assert.assertEquals("the number of statements should be 2601", 2601, model.size());
+
+		LOG.debug("read '" + model.size() + "' statements");
+
+		LOG.debug("finished read test for RDF resource at running DB");
 	}
 
 	@Test
 	public void testPing() throws IOException {
 
-		// POST the request
+		LOG.debug("start ping test for RDF resource at running DB");
+
 		final ClientResponse response = service().path("/rdf/ping").get(ClientResponse.class);
-		// System.out.println("Response Status : " + response.getStatus());
-		// System.out.println("Response body : " + response.getEntity(String.class));
 
 		Assert.assertEquals("expected 200", 200, response.getStatus());
 
 		final String body = response.getEntity(String.class);
 
 		Assert.assertEquals("expected pong", "pong", body);
+
+		LOG.debug("finished ping test for RDF resource at running DB");
+	}
+	
+	private void writeRDFToRunningDBInternal() throws IOException {
+		
+		LOG.debug("start writing RDF statements for RDF resource at running DB");
+		
+		final URL fileURL = Resources.getResource("dmpf_bsp1.n3");
+		final byte[] file = Resources.toByteArray(fileURL);
+
+		// Construct a MultiPart with two body parts
+		final MultiPart multiPart = new MultiPart();
+		multiPart.bodyPart(new BodyPart(file, MediaType.APPLICATION_OCTET_STREAM_TYPE)).bodyPart(
+				new BodyPart("http://data.slub-dresden.de/resources/2", MediaType.TEXT_PLAIN_TYPE));
+
+		// POST the request
+		final ClientResponse response = service().path("/rdf/put").type("multipart/mixed").post(ClientResponse.class, multiPart);
+
+		Assert.assertEquals("expected 200", 200, response.getStatus());
+
+		multiPart.close();
+		
+		LOG.debug("finished writing RDF statements for RDF resource at running DB");
 	}
 }

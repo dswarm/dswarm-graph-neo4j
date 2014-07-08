7f65372d5ce0da10fa9a3ef72b782f38f7cd4710
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-07-08 17:41:10 +0200

    [WIP][DD-393] continued delta/diff algo implementation

---
+++ src/main/java/org/dswarm/graph/gdm/work/PropertyEnrichGDMWorker.java
@@ -0,0 +1,166 @@
+package org.dswarm.graph.gdm.work;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.dswarm.graph.DMPGraphException;
+import org.dswarm.graph.NodeType;
+import org.dswarm.graph.json.LiteralNode;
+import org.dswarm.graph.json.Predicate;
+import org.dswarm.graph.json.ResourceNode;
+import org.dswarm.graph.json.Statement;
+import org.dswarm.graph.model.GraphStatics;
+import org.dswarm.graph.read.NodeHandler;
+import org.dswarm.graph.read.RelationshipHandler;
+import org.dswarm.graph.utils.GraphUtils;
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.ResourceIterable;
+import org.neo4j.graphdb.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author tgaengler
+ */
+public class PropertyEnrichGDMWorker implements GDMWorker {
+
+	private static final Logger						LOG	= LoggerFactory.getLogger(PropertyEnrichGDMWorker.class);
+
+	private final HierarchyLevelNodeHandler			nodeHandler;
+	private final NodeHandler						startNodeHandler;
+	private final HierarchyLevelRelationshipHandler	relationshipHandler;
+
+	private final String							resourceClassUri;
+	private final String							resourceUri;
+
+	private final GraphDatabaseService				database;
+
+	public PropertyEnrichGDMWorker(final String resourceClassUriArg, final String resourceUriArg, final GraphDatabaseService databaseArg) {
+
+		resourceClassUri = resourceClassUriArg;
+		resourceUri = resourceUriArg;
+		database = databaseArg;
+		nodeHandler = new CBDNodeHandler();
+		startNodeHandler = new CBDStartNodeHandler();
+		relationshipHandler = new CBDRelationshipHandler();
+	}
+
+	@Override
+	public void work() {
+
+		final Transaction tx = database.beginTx();
+
+		PropertyEnrichGDMWorker.LOG.debug("start enrich GDM TX");
+
+		try {
+
+			final Label recordClassLabel = DynamicLabel.label(resourceClassUri);
+
+			final ResourceIterable<Node> recordNodes = database.findNodesByLabelAndProperty(recordClassLabel, GraphStatics.URI_PROPERTY, resourceUri);
+
+			if (recordNodes == null) {
+
+				PropertyEnrichGDMWorker.LOG.debug("couldn't find records for resource '" + resourceUri + "' and resource class '" + resourceClassUri
+						+ "'");
+
+				return;
+			}
+
+			for (final Node recordNode : recordNodes) {
+
+				startNodeHandler.handleNode(recordNode);
+			}
+		} catch (final Exception e) {
+
+			PropertyEnrichGDMWorker.LOG.error("couldn't finished enrich GDM TX successfully", e);
+
+			tx.failure();
+			tx.close();
+		} finally {
+
+			PropertyEnrichGDMWorker.LOG.debug("finished enrich GDM TX finally");
+
+			tx.success();
+			tx.close();
+		}
+	}
+
+	private class CBDNodeHandler implements HierarchyLevelNodeHandler {
+
+		@Override
+		public void handleNode(final Node node, final int hierarchyLevel) throws DMPGraphException {
+
+			if (node.hasProperty(GraphStatics.RESOURCE_PROPERTY) && node.getProperty(GraphStatics.RESOURCE_PROPERTY).equals(resourceUri)) {
+
+				final Iterable<Relationship> relationships = node.getRelationships(Direction.OUTGOING);
+
+				for (final Relationship relationship : relationships) {
+
+					relationshipHandler.handleRelationship(relationship, hierarchyLevel);
+				}
+			}
+		}
+	}
+
+	private class CBDStartNodeHandler implements NodeHandler {
+
+		@Override
+		public void handleNode(final Node node) throws DMPGraphException {
+
+			if (node.hasProperty(GraphStatics.URI_PROPERTY)) {
+
+				final Iterable<Relationship> relationships = node.getRelationships(Direction.OUTGOING);
+
+				for (final Relationship relationship : relationships) {
+
+					relationshipHandler.handleRelationship(relationship, 0);
+				}
+			}
+		}
+	}
+
+	private class CBDRelationshipHandler implements HierarchyLevelRelationshipHandler {
+
+		@Override
+		public void handleRelationship(final Relationship rel, final int hierarchyLevel) throws DMPGraphException {
+
+			final long statementId = rel.getId();
+
+			// subject
+
+			final Node subjectNode = rel.getStartNode();
+			subjectNode.setProperty("__HIERARCHY_LEVEL__", hierarchyLevel);
+
+			// predicate
+
+			rel.setProperty("__HIERARCHY_LEVEL__", hierarchyLevel);
+
+			// object
+
+			final Node objectNode = rel.getEndNode();
+			final NodeType objectNodeType = GraphUtils.determineNodeType(objectNode);
+
+			switch (objectNodeType) {
+
+				case Literal:
+				case Resource:
+				case TypeResource:
+
+					objectNode.setProperty("__HIERARCHY_LEVEL__", hierarchyLevel + 1);
+
+					break;
+				default:
+
+					// continue traversal with object node
+					nodeHandler.handleNode(rel.getEndNode(), hierarchyLevel + 1);
+
+					break;
+			}
+		}
+	}
+}

f873fc3a0bee0b3678d4935caffedb034c7518b7
Thomas Gängler <tgaengler@avantgarde-labs.de>
2014-09-29 10:24:17 +0200

    switched hashing algo to siphash + temp cache index hits again

---
+++ src/main/java/org/dswarm/graph/asd.java
@@ -0,0 +1,8 @@
+package org.dswarm.graph;
+
+/**
+ * Created by tgaengler on 25/09/14.
+ */
+public class asd {
+
+}
---
+++ src/main/java/org/dswarm/graph/hash/HashUtils.java
@@ -0,0 +1,8 @@
+package org.dswarm.graph.hash;
+
+/**
+ * Created by tgaengler on 29/09/14.
+ */
+public class HashUtils {
+
+}

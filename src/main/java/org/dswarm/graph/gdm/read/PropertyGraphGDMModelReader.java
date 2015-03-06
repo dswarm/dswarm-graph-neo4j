4faff11e2d85646b7b8cac2cf2c0c2f25845cf14
Paul Horn <knutwalker@gmail.com>
2015-03-06 17:35:19 +0100

    Fix license header

--- src/main/java/org/dswarm/graph/gdm/read/PropertyGraphGDMModelReader.java
+++ src/main/java/org/dswarm/graph/gdm/read/PropertyGraphGDMModelReader.java
@@ -1,10 +1,18 @@
 /**
- * This file is part of d:swarm graph extension. d:swarm graph extension is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version. d:swarm graph extension is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details. You should have received a copy of the GNU General Public License along with d:swarm
- * graph extension. If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of d:swarm graph extension.
+ *
+ * d:swarm graph extension is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * d:swarm graph extension is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with d:swarm graph extension.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.dswarm.graph.gdm.read;
 
@@ -183,7 +191,7 @@ public class PropertyGraphGDMModelReader implements GDMModelReader {
 
 			while (nodeIterator.hasNext()) {
 
-				final Node recordNode = recordNodesIter.next();
+				final Node recordNode = nodeIterator.next();
 				final String resourceUri = (String) recordNode.getProperty(GraphStatics.URI_PROPERTY, null);
 
 				if (resourceUri == null) {

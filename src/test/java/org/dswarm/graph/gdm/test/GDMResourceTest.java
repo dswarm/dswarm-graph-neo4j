edd45f32edb5fba4d3663ba24dd062c7ea30035e
Thomas Gängler <tgaengler@avantgarde-labs.de>
2015-03-23 17:02:39 +0100

    [DD-931] enhanced write GDM endpoint

--- src/test/java/org/dswarm/graph/gdm/test/GDMResourceTest.java
+++ src/test/java/org/dswarm/graph/gdm/test/GDMResourceTest.java
@@ -52,9 +52,14 @@ public abstract class GDMResourceTest extends BasicResourceTest {
 
 	private static final String	DEFAULT_GDM_FILE_NAME	= "test-mabxml.gson";
 
+	private final ObjectMapper	objectMapper;
+
 	public GDMResourceTest(final Neo4jDBWrapper neo4jDBWrapper, final String dbTypeArg) {
 
 		super(neo4jDBWrapper, "/gdm", dbTypeArg);
+
+		objectMapper = Util.getJSONObjectMapper();
+		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 	}
 
 	@Test
@@ -164,7 +169,7 @@ public abstract class GDMResourceTest extends BasicResourceTest {
 		final String recordClassURI = "http://www.ddb.de/professionell/mabxml/mabxml-1.xsd#datensatzType";
 		final int numberOfStatements = 191;
 
-		readGDMFromDB(recordClassURI, dataModelURI, numberOfStatements, Optional.<Integer> absent());
+		readGDMFromDB(recordClassURI, dataModelURI, numberOfStatements, Optional.<Integer>absent());
 
 		LOG.debug("finished read test for GDM resource at " + dbType + " DB");
 	}
@@ -248,10 +253,16 @@ public abstract class GDMResourceTest extends BasicResourceTest {
 		final URL fileURL = Resources.getResource(fileName);
 		final byte[] file = Resources.toByteArray(fileURL);
 
+		final ObjectNode metadata = objectMapper.createObjectNode();
+		metadata.put(DMPStatics.DATA_MODEL_URI_IDENTIFIER, dataModelURI);
+
+		final String requestJsonString = objectMapper.writeValueAsString(metadata);
+
+
 		// Construct a MultiPart with two body parts
 		final MultiPart multiPart = new MultiPart();
 		multiPart.bodyPart(new BodyPart(file, MediaType.APPLICATION_OCTET_STREAM_TYPE)).bodyPart(
-				new BodyPart(dataModelURI, MediaType.TEXT_PLAIN_TYPE));
+				new BodyPart(requestJsonString, MediaType.APPLICATION_JSON_TYPE));
 
 		// POST the request
 		final ClientResponse response = target().path("/put").type("multipart/mixed").post(ClientResponse.class, multiPart);
